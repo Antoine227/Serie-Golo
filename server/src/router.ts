6e15e1d4ade8acc3cdb03af40915144d58692a66
ANTOINE CHAUVOT <antoine_chauvot@hotmail.com>
2025-01-06 12:12:22 +0100

    quête 4 routes program et category

--- server/src/router.ts
+++ server/src/router.ts
@@ -13,6 +13,20 @@ router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
 
+// Declaration of a "Programs" route
+
+import programActions from "./modules/program/programActions";
+
+router.get("/api/programs", programActions.browse);
+router.get("/api/programs/:id", programActions.read);
+
+// Declaration of a "Programs" route
+
+import categoryActions from "./modules/category/categoryActions";
+
+router.get("/api/categories", categoryActions.browse);
+router.get("/api/categories/:id", categoryActions.read);
+
 /* ************************************************************************* */
 
 // Declaration of a "Welcome" route
@@ -20,12 +34,6 @@ router.post("/api/items", itemActions.add);
 import sayActions from "./modules/say/sayActions";
 
 router.get("/", sayActions.sayWelcome);
-
-// Declaration of a "Programs" route
-
-import programActions from "./modules/program/programActions";
-
-router.get("/api/programs", programActions.browse);
 
 /* ************************************************************************* */
 
